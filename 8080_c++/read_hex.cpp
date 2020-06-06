855f266087d9f450e1762ac5ffd4351a56b90073
moriyaprateek <43816358+moriyaprateek@users.noreply.github.com>
2020-06-06 13:03:16 +0530

    Update read_hex.cpp

--- 8080_c++/read_hex.cpp
+++ 8080_c++/read_hex.cpp
@@ -5,6 +5,57 @@ using namespace std;
 
 
 
+
+typedef struct ConditionCodes {
+
+	uint8_t z:1;
+	uint8_t s:1;
+	uint8_t p:1;
+	uint8_t cy:1;
+	uint8_t ac:1;
+	uint8_t pad:3;
+} ConditionCodes;
+
+typedef struct State8080{
+	uint8_t a;
+	uint8_t b;
+	uint8_t c;
+	uint8_t d;
+	uint8_t e;
+	uint8_t h;
+	uint8_t l;
+	uint16_t sp;
+	uint16_t pc;
+	uint8_t *memory;
+	struct ConditionCodes cc;
+	uint8_t int_enable;
+} State8080;
+
+//For an error call for an invalid instruction/opcode
+void UnimplementedInstruction(State8080* state){
+	printf("Error: Unimplemented instruction!\n")
+	exit(1);
+}
+
+
+void Emulate8080Op(State8080* state){
+	unsigned char *opcode = &state->memory[state->pc]; //It's a pointer that points to the PC
+
+	switch(*opcode){
+		case 0x00:
+			break;
+		case 0x01:
+			state->pc = opcode[1]; //LXI   B, word == Load the register pair immediate. So the address of the second part, i.e, word is copied to the B(L?) pair
+			state->pc = opcode[2];
+			state->pc += 2;
+			break;
+	}
+	state->pc += 1; //for the opcode? so add 1 to the PC to get the next opcode?
+}
+
+
+
+
 int Disassemble8080Op(unsigned char *codebuffer, int pc)
 {
 	unsigned char *code = &codebuffer[pc];
